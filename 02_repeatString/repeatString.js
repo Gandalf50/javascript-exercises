ad61e1bc5ffc0e9cce548ef2e78b9f436314563e
Gandalf50 <bbaggins.shire50@gmail.com>
2022-02-15 01:01:05 -0300

    Complete excersices 1 to 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(hey='',num) {
+    if (!hey) {
+        return '';
+    } else if (num==0) {
+        return '';
+    } else if (num<0) {
+        return 'ERROR'
+    } else {
+        for (i=1;i<num;i++) {
+            hey += 'hey';
+        }
+        return hey
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    if (!text) {
+        return '';
+    } else {
+        let reversed = text[text.length-1];
+        for (i=text.length-2;i>=0;i--) {
+            reversed += text[i];
+        }
+        return reversed;
+    }
 };
 
 // Do not edit below this line
